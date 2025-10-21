cd2e290cebfad3c6f6d54286a8a5ca1f4a575623
k1llm3sixy <62442843+k1llm3sixy@users.noreply.github.com>
2025-10-21 21:50:31 +0300

    optimization

--- complex_math_2.lua
+++ complex_math_2.lua
@@ -1,15 +1,19 @@
 local runService = game:GetService("RunService")
 local player = game.Players.LocalPlayer
 local hmdRoot = player.Character:WaitForChild("HumanoidRootPart")
+local parts = {}
 
 player.CharacterAdded:Connect(function(character)
     hmdRoot = character:WaitForChild("HumanoidRootPart")
 end)
 
-runService.RenderStepped:Connect(function()
-    for _, part in pairs(workspace:GetDescendants()) do
-        if part.Name ~= "NoKill" then continue end
+for _, part in pairs(workspace:GetDescendants()) do
+    if part.Name ~= "NoKill" then continue end
+    table.insert(parts, part)
+end
 
+runService.RenderStepped:Connect(function()
+    for _, part in ipairs(parts) do
         local distance = math.floor((hmdRoot.Position - part.Position).Magnitude)
         local highlight = part:FindFirstChildOfClass("Highlight")
 
